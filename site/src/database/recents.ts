2d861b3095d9b9d9e47fffa2904c1e08aefd2a75
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-06 16:45:58 -0300

    Criado arquivo recents.ts

---
+++ site/src/database/recents.ts
@@ -0,0 +1,26 @@
+export const recents = [
+  {
+    id: 1,
+    productName: 'Camiseta Branca Básica',
+    productDescription:
+      'Uma camiseta básica em 100% algodão, perfeita para o dia a dia. Confortável e versátil, é uma peça essencial em qualquer guarda-roupa.',
+    price: 29.99,
+    category: 'camisetas',
+  },
+  {
+    id: 2,
+    productName: 'Calça Jeans',
+    productDescription:
+      'Calça jeans modelo skinny, proporcionando um visual moderno e elegante. Ideal para diversas ocasiões, do casual ao mais formal.',
+    price: 59.99,
+    category: 'calças',
+  },
+  {
+    id: 3,
+    productName: 'Vestido Floral',
+    productDescription:
+      'Vestido com estampa floral delicada, perfeito para ocasiões especiais. Combine estilo e feminilidade com esta peça encantadora.',
+    price: 39.99,
+    category: 'vestidos',
+  },
+];
