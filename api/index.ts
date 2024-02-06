50d9db022a828e812f2ab62c457aed71a835c35c
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-06 18:54:54 -0300

    Feito alterações no código

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+Bun.serve({
+  port: 3000,
+  fetch(req){
+    const url = new URL(req.url)
 
-const app = express();
+    if(url.pathname === '/') return new Response('Home Page')
+    if(url.pathname === '/login') return new Response('Login Page')
+    if(url.pathname === '/register') return new Response('Register Page')
+    if(url.pathname === '/profile') return new Response('Profile Page')
+    if(url.pathname === '/about') return new Response('About Page')
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000')
-});
+    return new Response('404')
+  }
+})
+
+console.log('API Ativada')
