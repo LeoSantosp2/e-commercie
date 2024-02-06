2439cae1e68d6c63432745f25d5844d0ebafd028
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-06 16:45:43 -0300

    Criado arquivo emphasis.ts

---
+++ site/src/database/emphasis.ts
@@ -0,0 +1,50 @@
+export const emphasis = [
+  {
+    id: 1,
+    productName: 'Camiseta Branca Básica',
+    productDescription:
+      'Uma camiseta básica em 100% algodão, perfeita para o dia a dia. Confortável e versátil, é uma peça essencial em qualquer guarda-roupa.',
+    price: 29.99,
+    category: 'camisetas',
+  },
+  {
+    id: 2,
+    productName: 'Calça Jeans',
+    productDescription:
+      'Calça jeans modelo skinny, proporcionando um visual moderno e elegante. Ideal para diversas ocasiões, do casual ao mais formal.',
+    price: 59.99,
+    category: 'calças',
+  },
+  {
+    id: 3,
+    productName: 'Vestido Floral',
+    productDescription:
+      'Vestido com estampa floral delicada, perfeito para ocasiões especiais. Combine estilo e feminilidade com esta peça encantadora.',
+    price: 39.99,
+    category: 'vestidos',
+  },
+  {
+    id: 4,
+    productName: 'Moletom com Capuz',
+    productDescription:
+      'Moletom confortável com capuz, ideal para os dias mais frios. Garanta estilo e aquecimento com esta peça casual.',
+    price: 49.99,
+    category: 'moletoms',
+  },
+  {
+    id: 5,
+    productName: 'Saia Midi',
+    productDescription:
+      'Saia midi elegante e versátil, perfeita para criar looks modernos. Uma adição versátil ao seu guarda-roupa.',
+    price: 34.99,
+    category: 'saias',
+  },
+  {
+    id: 6,
+    productName: 'Blusa de Lã',
+    productDescription:
+      'Blusa de lã quente e aconchegante, ideal para os meses mais frios. Estilo e conforto combinados em uma única peça.',
+    price: 44.99,
+    category: 'blusas',
+  },
+];
