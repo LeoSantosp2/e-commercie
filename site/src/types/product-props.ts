c9e6e4364eac6fde7fd5aa439e37039f06fa1ce5
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-06 16:46:46 -0300

    Criado tipo para produto

---
+++ site/src/types/product-props.ts
@@ -0,0 +1,12 @@
+export type ProductProps = {
+  id: number;
+  productName: string;
+  productDescription: string;
+  price: number;
+  category: string;
+  qtd?: string;
+};
+
+export type Product = {
+  id: number;
+};
