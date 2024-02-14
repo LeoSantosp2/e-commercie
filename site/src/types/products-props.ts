9747d31f0014bd6a61a25fc4be7861fde2a6ecfa
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-14 11:50:26 -0300

    Criado tipo para products

---
+++ site/src/types/products-props.ts
@@ -0,0 +1,10 @@
+export type ProductsProps = {
+  id: string;
+  product_name: string;
+  product_description: string;
+  price: number;
+  category: string;
+  stock: string;
+  created_at: string;
+  updated_at: string;
+};
