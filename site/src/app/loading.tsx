ec4de75c507e55707baeb7f23d41e4c76a01f5e9
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-14 11:49:38 -0300

    Criado Loading

---
+++ site/src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="size-full flex justify-center items-center">
+      <p className="text-4xl">Carregando...</p>
+    </div>
+  );
+}
