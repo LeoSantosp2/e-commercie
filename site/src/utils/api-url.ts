c797231da42a5b22e0bc16d36326408ecbb46273
Leonardo Santos <leogilberto2010@hotmail.com>
2024-02-17 00:01:39 -0300

    Criado função para centralizar a url da api mudando apenas os paramêtros para acessar alguns caminhos da url da api

---
+++ site/src/utils/api-url.ts
@@ -0,0 +1,7 @@
+export const apiUrl = async (pathname: string) => {
+  const response = await fetch(`http://localhost:3005${pathname}`);
+
+  const datas = await response.json();
+
+  return datas.data;
+};
